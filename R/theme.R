52bbc3052cbea00fdbfc9bb370a0d7efb221293a
Italo Cegatta <italocegatta@gmail.com>
2023-08-15 09:30:49 -0300

    graficos 1 e 2

--- R/theme.R
+++ R/theme.R
@@ -1,13 +1,11 @@
-
+# Theme for internal plots
 #' @export
-theme_custom <- function(font_size = 14, font_family = "sans") {
+theme_custom <- function(base_size = 11, base_family = "") {
 
-  ggplot2::theme_bw(font_size, font_family) %+replace%
-    theme(
+  ggplot2::theme_bw(base_size, base_family) %+replace%
+    ggplot2::theme(
       text = ggplot2::element_text(color = "black"),
       axis.text = ggplot2::element_text(color = "black"),
-      # panel.grid.major = ggplot2::element_blank(),
-      # panel.grid.minor = ggplot2::element_blank(),
       plot.margin = ggplot2::margin(t = 10, r = 10, b = 10, l = 10, unit = "pt")
     )
 }
